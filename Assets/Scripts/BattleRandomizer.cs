d26113f54c49dea7defb85c9d8b18645e0890ed4
JezrienJDR <JDRwrites@gmail.com>
2020-10-19 17:53:57 -0400

    Not sure if I've pushed these changes

---
+++ Assets/Scripts/BattleRandomizer.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BattleRandomizer : MonoBehaviour
+{
+    private float timeElapsed;
+
+    public float checkTime;
+
+    public float odds;
+
+    public GameObject player;
+
+    BoxCollider2D area;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeElapsed = 0;
+
+        area = GetComponent<BoxCollider2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 playerPos = new Vector2(player.transform.position.x, player.transform.position.y);
+
+        if (playerPos == area.ClosestPoint(player.transform.position))
+        {
+            timeElapsed += Time.deltaTime;
+
+            if (timeElapsed >= checkTime)
+            {
+                timeElapsed = 0;
+
+                float roll = Random.Range(0.0f, 100.0f);
+
+                if (roll <= odds)
+                {
+                    SceneManager.LoadScene("BattleScene");
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    char direction = 'L';
+
+    SpriteRenderer sr;
+
+    [SerializeField]
+    float speed = 1;
+
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("up") | Input.GetKey("w"))
+        {
+            direction = 'U';
+        }
+
+        else if (Input.GetKey("down") | Input.GetKey("s"))
+        {
+            direction = 'D';
+        }
+
+        else if (Input.GetKey("left") | Input.GetKey("a"))
+        {
+            direction = 'L';
+
+            sr.flipX = false;
+        }
+
+        else if (Input.GetKey("right") | Input.GetKey("d"))
+        {
+            direction = 'R';
+            sr.flipX = true;
+        }
+
+        else
+        {
+            direction = 'N';
+        }
+
+        float moveSpeed = speed * Time.deltaTime;
+
+        switch (direction)
+        {
+            case 'U':
+                {
+                    transform.Translate(new Vector3(0, moveSpeed, 0));
+                    break;
+                }
+            case 'D':
+                {
+                    transform.Translate(new Vector3(0, -moveSpeed, 0));
+                    break;
+                }
+            case 'L':
+                {
+                    transform.Translate(new Vector3(-moveSpeed, 0, 0));
+                    break;
+                }
+            case 'R':
+                {
+                    transform.Translate(new Vector3(moveSpeed, 0, 0));
+                    break;
+                }
+            default:
+                {
+                    break;
+                }
+
+        }
+
+    }
+}
